be3db11254f0d43e4c4cc04a8c0cbed0c725d60f
Miguel Paysan <Miguel_Paysan@intuit.com>
2015-05-08 20:10:05 -0700

    Begin place details page

--- public/js/ng-app.js
+++ public/js/ng-app.js
@@ -31,6 +31,11 @@ angular.module('nightrouletteApp', [
      	templateUrl : 'partials/about.html',
      	controller : 'MainCtrl'
      })
+     .state('place', {
+        url : '/place/:id',
+        templateUrl : 'partials/placedetails.html',
+        controller : 'PlacesCtrl'
+     })
 	;
 
      $urlRouterProvider.otherwise ('/');
@@ -67,6 +72,7 @@ angular.module('nightrouletteApp', [
     var res = {};
 
     var r0 = {
+        'id':0,
         'name' : 'Incahoots',
         'img' : 'http://clubworld360.com/data/venues/1442/full_InCahoots%20-%20Copy.jpg',
         'tagline' : 'Yeehaw country line dancing!',
@@ -77,6 +83,7 @@ angular.module('nightrouletteApp', [
     };
 
     var r1 = {
+        'id':1,
         'name' : 'Tavern',
         'img' : 'https://bakaresd.files.wordpress.com/2010/02/tavern1.jpg',
         'tagline' : 'Downtown SD at PB',
@@ -87,6 +94,7 @@ angular.module('nightrouletteApp', [
     };
 
     var r2 = {
+        'id':2,
         'name' : 'McDonalds',
         'img' : 'http://www.burgerbusiness.com/wp-content/uploads/McD_LovinIt.png',
         'tagline' : 'This aint no Buuurger Kiing',
@@ -97,6 +105,7 @@ angular.module('nightrouletteApp', [
     };
     
     var r3 = {
+        'id':3,
         'name' : 'Mira Mesa Bowling Lanes',
         'img' : 'http://1.bp.blogspot.com/-2QBgwLy97U4/U8O1qlCVbKI/AAAAAAAAAas/sxb_QRb1efo/s1600/strike_400_wht_7927.png',
         'tagline' : 'Get a 300, just like Mark Herrero',
@@ -107,6 +116,7 @@ angular.module('nightrouletteApp', [
     }; 
 
     var r4 = {
+        'id':4,
         'name' : 'Surfing',
         'img' : 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRUHOXTjRq50CC8PbJ8t2w3lUOF9_-LsULBJSmvbc-vH6sE7jrx4TIZhB7i',
         'tagline' : 'Cowa bunga',
@@ -117,6 +127,7 @@ angular.module('nightrouletteApp', [
     }; 
 
     var r5 = {
+        'id':5,
         'name' : 'Brewskis',
         'img' : 'http://www.brewskissd.com/images/brewskis.png',
         'tagline' : 'Free arcade games, cheap dances at Goldfingers',
@@ -127,6 +138,7 @@ angular.module('nightrouletteApp', [
     }; 
 
     var r6 = {
+        'id':6,
         'name' : 'PB shore club',
         'img' : 'http://www.sandiegan.com/images/Restaurants/pbshoreclub.jpg',
         'tagline' : 'Red bull Vodka slushis RBVs',
@@ -166,6 +178,20 @@ res['places'] = placeArr;
         $scope.places = placeFactory['places'].slice(3,6);
     }
 
+    $scope.getPlaceDetails = function (id) {
+        $state.go('place', {'id':id});
+    }
     console.log(total);
  }])
+
+
+.controller('PlacesCtrl', ['$scope', '$state', '$stateParams', 'PlaceFactory',function ($scope, $state, $stateParams, placeFactory) {
+    var placeId = $stateParams['id'];
+    $scope.place = $.grep(placeFactory['places'], function(e){ return e.id == placeId; })[0];
+
+    $scope.toggleViews = function (elem) {
+        $(elem).toggleClass('hide');
+    }
+ }])
+
 ;
